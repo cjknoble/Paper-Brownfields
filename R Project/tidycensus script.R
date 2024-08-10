5f044f7ceaf59b56e13925a9c608402c58c222f5
Charles 'CJ' Knoble <139992784+cjknoble@users.noreply.github.com>
2024-08-10 10:48:25 -0400

    Update tidycensus script.R

--- R Project/tidycensus script.R
+++ R Project/tidycensus script.R
@@ -8,7 +8,7 @@ library(stringr)
 library(ggplot2)
 
 # Set Census API key (only needed once)
-#census_api_key("a785070a2d252fd0d07b81912b3bb437cbed9ba2", install = TRUE)
+#census_api_key("api", install = TRUE)
 
 # Check all variables to select most appropriate  
 v22 <- load_variables(2022, "acs5", cache = TRUE)
